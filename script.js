2004d5296e3061d80d3ccc2bd571a59bb41f2eef
Lewis Lawler <lewislawler98@gmail.com>
2024-01-17 16:50:55 +0000

    adds click functionality

--- script.js
+++ script.js
@@ -7,12 +7,24 @@ const playAgainButton = document.querySelector(".play-again");
 const totalCells = 100;
 const totalBombs = 90;
 const maxScore = 5;
-const bombsList = [];  // Fixed typo here
+const bombsList = [];
 
 let score = 0;
 
 for (let i = 1; i <= 100; i++) {
   const cell = document.createElement("div");
   cell.classList.add("cell");
+
+  cell.addEventListener("click", function () {
+    cell.classList.add("cell-clicked");
+  });
   grid.appendChild(cell);
+}
+
+while (bombsList.length < totalBombs) {
+  const randomNumber = Math.floor(Math.random() * totalCells) + 1;
+
+  if (!bombsList.includes(randomNumber)) {
+    bombsList.push(randomNumber);
+  }
 }
